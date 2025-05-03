e5d7a096c4360dc1fbbded1c83d52bafe3f2ebab
mdiftakharislam <iftakharislam1234@gmail.com>
2025-05-03 18:02:59 +0600

    Commit changes

--- iiucauditorium 1/iiucaud01.cpp
+++ iiucauditorium 1/iiucaud01.cpp
@@ -3,41 +3,43 @@
 
  class IIUC_Auditorium {
  private:
-     float area;
-     float totalFloorSpace;
      int hallID;
-     int maxSeatingCapacity;
+     int maxcapacity;
+     float area;
+     float totalSpace;
 
  public:
-     IIUC_Auditorium(float a, float tfs, int id, int capacity) {
-         area = a;
-         totalFloorSpace = tfs;
+     IIUC_Auditorium( int id, int capacity,float a, float s) {
          hallID = id;
-         maxSeatingCapacity = capacity;
+         maxcapacity = capacity;
+         area = a;
+         totalSpace = s;
+         
      }
 
 
-     IIUC_Auditorium(const IIUC_Auditorium &other) {
-         area = other.area;
-         totalFloorSpace = other.totalFloorSpace;
-         hallID = other.hallID;
-         maxSeatingCapacity = other.maxSeatingCapacity;
+     IIUC_Auditorium(const IIUC_Auditorium &Auditorium) {
+         hallID =Auditorium.hallID;
+         maxcapacity = Auditorium.maxcapacity;
+         area =Auditorium.area;
+         totalSpace = Auditorium.totalSpace;
+         
      }
 
 
      void display() {
+         cout << "Total Floor Space: " << totalSpace  << endl;
+         cout << "Maximum Seating Capacity: " << maxcapacity  << endl;
          cout << "Hall ID: " << hallID << endl;
-         cout << "Area: " << area << " sq.ft" << endl;
-         cout << "Total Floor Space: " << totalFloorSpace << " sq.ft" << endl;
-         cout << "Maximum Seating Capacity: " << maxSeatingCapacity << " participants" << endl;
-         cout << "----------------------------------------" << endl;
+         cout << "Area: " << area << endl;
+         
      }
  };
 
  int main() {
 
-     IIUC_Auditorium hall1(4500.0, 4000.0, 101, 350);
-     IIUC_Auditorium hall2(6000.0, 5500.0, 102, 500);
+     IIUC_Auditorium hall1(6000, 3000, 350, 650);
+     IIUC_Auditorium hall2(7000, 4000, 400,700);
 
 
      IIUC_Auditorium hall3 = hall1;
